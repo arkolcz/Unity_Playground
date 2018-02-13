a3038c4b6b8b3ecbabb0fee45fb2841031da4d98
RoXKhaar <9158703+RoXKhaar@users.noreply.github.com>
2018-02-13 22:20:04 +0100

    Added splash screen and basic main menu layout and functionalities

---
+++ FPS_WSIU/Assets/Scripts/SplashScreenManager.cs
@@ -0,0 +1,33 @@
+﻿/*
+Made by RoXKhaar
+*/
+
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SplashScreenManager : MonoBehaviour
+{
+    public Image splashImage;
+
+    IEnumerator Start()
+    {
+        splashImage.canvasRenderer.SetAlpha(0.0f);
+
+        FadeIn();
+        yield return new WaitForSeconds(2.5f);
+        FadeOut();
+        yield return new WaitForSeconds(2.5f);
+
+    }
+
+    private void FadeIn()
+    {
+        splashImage.CrossFadeAlpha(1.0f, 1.5f, false);
+    }
+
+    private void FadeOut()
+    {
+        splashImage.CrossFadeAlpha(0.0f, 2.5f, false);
+    }
+}
