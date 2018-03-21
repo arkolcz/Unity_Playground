86f279dd1157456094e9e458054e1b4ea9b2ce27
RoXKhaar <9158703+RoXKhaar@users.noreply.github.com>
2018-03-21 23:46:55 +0100

    Redesign of Main Menu

--- FPS_WSIU/Assets/Scripts/MenuManager.cs
+++ FPS_WSIU/Assets/Scripts/MenuManager.cs
@@ -7,20 +7,23 @@ using UnityEngine;
 public class MenuManager : MonoBehaviour {
 
     public GameObject settingsMenu;
+    public GameObject mainMenu;
 
     public void Play()
     {
         // TODO: Load Game Scene
     }
 
-    public void SettingsTabManager()
+    public void MenuTabManager()
     {
         if(settingsMenu.activeInHierarchy == true)
         {
             settingsMenu.SetActive(false);
+            mainMenu.SetActive(true);
         }
         else
         {
+            mainMenu.SetActive(false);
             settingsMenu.SetActive(true);
         }
     }
